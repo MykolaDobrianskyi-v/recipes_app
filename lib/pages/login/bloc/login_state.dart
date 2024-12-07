7d00ecaf7dc80c7fc81e504fc37d585973fb4d85
MykolaDobrianskyi <mikoladobriy@gmail.com>
2024-12-07 19:49:00 +0200

    login/validation

--- lib/pages/login/bloc/login_state.dart
+++ lib/pages/login/bloc/login_state.dart
@@ -1,16 +1,37 @@
 part of 'login_bloc.dart';
 
-enum LoginStatus {
-  init,
-  loading,
-}
-
 class LoginState extends Equatable {
   final String email;
   final String password;
+  final bool isEmailValid;
+  final bool isPasswordValid;
+  final String? errorMessage;
 
-  const LoginState({this.email = '', this.password = ''});
+  const LoginState({
+    this.email = '',
+    this.password = '',
+    this.isEmailValid = true,
+    this.isPasswordValid = true,
+    this.errorMessage,
+  });
+
+  LoginState copyWith({
+    String? email,
+    String? password,
+    bool? isEmailValid,
+    bool? isPasswordValid,
+    String? errorMessage,
+  }) {
+    return LoginState(
+      email: email ?? this.email,
+      password: password ?? this.password,
+      isEmailValid: isEmailValid ?? this.isEmailValid,
+      isPasswordValid: isPasswordValid ?? this.isPasswordValid,
+      errorMessage: errorMessage,
+    );
+  }
 
   @override
-  List<Object?> get props => [email, password];
+  List<Object?> get props =>
+      [email, password, isEmailValid, isPasswordValid, errorMessage];
 }
