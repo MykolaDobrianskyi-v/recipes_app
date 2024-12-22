24ae0eae3b47289b1ab4f2f00958bd7c20895df9
MykolaDobrianskyi <mikoladobriy@gmail.com>
2024-12-22 17:46:45 +0200

    Merge pull request #6 from MykolaDobrianskyi-v/feature/fetch_ingredient_categories
    Feature/fetch ingredient categories

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:chat_app/constants/text_data/text_constants.dart';
 import 'package:chat_app/pages/home/bloc/home_bloc.dart';
+import 'package:chat_app/pages/ingredients/ingredients_page.dart';
+import 'package:chat_app/repositories/ingredients_categories_repository.dart';
 import 'package:chat_app/widgets/ingredients_container.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -19,39 +21,54 @@ class HomePage extends StatelessWidget {
           centerTitle: true,
           backgroundColor: const Color(0xFFE37070),
         ),
-        bottomNavigationBar: BlocBuilder<HomeBloc, HomeState>(
-          builder: (context, state) {
-            return BottomNavigationBar(
-                backgroundColor: const Color(0xFFCA9F9F),
-                selectedItemColor: const Color(0xFFE37070),
-                currentIndex: state.selectedIndex ?? 0,
-                onTap: (index) => context
-                    .read<HomeBloc>()
-                    .add(OnItemTapped(selectedIndex: index)),
-                items: const [
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.abc),
-                    label: '',
-                  ),
-                  BottomNavigationBarItem(icon: Icon(Icons.abc), label: ''),
-                  BottomNavigationBarItem(icon: Icon(Icons.abc), label: ''),
-                ]);
-          },
-        ),
+        // bottomNavigationBar: BlocBuilder<HomeBloc, HomeState>(
+        //   builder: (context, state) {
+        //     return BottomNavigationBar(
+        //         backgroundColor: const Color(0xFFCA9F9F),
+        //         selectedItemColor: const Color(0xFFE37070),
+        //         currentIndex: state.selectedIndex ?? 0,
+        //         onTap: (index) => context
+        //             .read<HomeBloc>()
+        //             .add(OnItemTapped(selectedIndex: index)),
+        //         items: const [
+        //           BottomNavigationBarItem(
+        //             icon: Icon(Icons.abc),
+        //             label: '',
+        //           ),
+        //           BottomNavigationBarItem(icon: Icon(Icons.abc), label: ''),
+        //           BottomNavigationBarItem(icon: Icon(Icons.abc), label: ''),
+        //         ]);
+        //   },
+        // ),
         body: Padding(
           padding: const EdgeInsets.only(top: 170),
           child: BlocBuilder<HomeBloc, HomeState>(
             builder: (context, state) {
+              if (state.isLoading ?? false) {
+                return const Center(
+                  child: CircularProgressIndicator(
+                    color: Color(0xFFCA9F9F),
+                  ),
+                );
+              }
               return GridView.count(
-                  mainAxisSpacing: 10,
-                  crossAxisSpacing: 10,
-                  crossAxisCount: 3,
-                  children: [
-                    for (final category in state.ingredientCategory)
-                      IngredientsContainer(
-                          imagePath: category.imagePath,
-                          containerText: category.label),
-                  ]);
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 10,
+                crossAxisCount: 3,
+                children: [
+                  for (final category in state.ingredientCategory)
+                    IngredientsContainer(
+                      imageUrl: category.imageUrl,
+                      containerText: category.label,
+                      onTap: () {
+                        Navigator.of(context).pushNamed(
+                          IngredientsPage.routeName,
+                        );
+                      },
+                    ),
+                ],
+              );
+
             },
           ),
         ),
